01c48248b1c195f9977fbd33b32b0b9adabab7d9
Blake Burkhart <blake.burkhart@us.af.mil>
2021-09-02 15:07:17 -0500

    update

---
+++ Red-Hat-securitydata-CVE/Code.js
@@ -0,0 +1,22 @@
+function doGet(e) {
+  const feed = {
+    "version": "https://jsonfeed.org/version/1",
+    "title": `RedHat ${e.parameter.package} CVEs`,
+    "home_page_url": "https://access.redhat.com/hydra/rest/securitydata/cve.json",
+    "items": []
+  }
+
+  const content = UrlFetchApp.fetch(feed.home_page_url+"?"+e.queryString).getContentText();
+  const json = JSON.parse(content);
+  feed.items = json.map(item => {
+    return {
+        "id": item.CVE,
+        "url": `https://access.redhat.com/security/cve/${item.CVE}`,
+        "title": item.bugzilla_description,
+        "content_text": item.bugzilla_description,
+        "date_published": item.public_date,
+    }
+  });
+
+  return ContentService.createTextOutput(JSON.stringify(feed)).setMimeType(ContentService.MimeType.JSON);
+}
