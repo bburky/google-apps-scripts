35a35582b62c03ec6cd5f2a51d0520abd3ced265
Blake Burkhart <blake.burkhart@us.af.mil>
2021-09-02 20:56:03 -0500

    sonatype: use rollup and typescript

--- sonatype/main.ts
+++ sonatype/main.ts
@@ -1,22 +1,24 @@
-//Cheeriogs library 1ReeQ6WO8kKNxoaA_O0XEQ589cIrRvEBA9qcWpNqdOP17i47u6N9M5Xh0
+import { JSONFeed, JSONFeedItem } from '../jsonschema'
+import cheerio from 'cheerio';
+
+export default doGet;
 
 function doGet() {
-  // JSON Feed https://jsonfeed.org/version/1
-  const feed = {
+  const feed: JSONFeed = {
     "version": "https://jsonfeed.org/version/1",
     "title": "Sonatype Security Advisories",
     "home_page_url": "https://support.sonatype.com/hc/en-us/sections/203012668-Security-Advisories",
-    "items": []
+    "items": [],
   }
 
   const content = UrlFetchApp.fetch(feed.home_page_url).getContentText();
-  const $ = Cheerio.load(content);
+  const $ = cheerio.load(content);
   const posts = $(".article-list a");
   feed.items = posts.map(function(i, el) {
     const $el = $(el);
     const url = "https://support.sonatype.com" + $el.attr('href');
     const match = /.* - (\d{4}-\d\d-\d\d)/.exec($el.text());
-    const ret =  {
+    const ret: JSONFeedItem = {
       "id": url,
       "url": url,
       "title": $el.text(),
